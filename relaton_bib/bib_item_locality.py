c08a57ac4ab4ce17c9c6bd0cae436d109cfa3db5
Alexande B <abobrikovich@gmail.com>
2021-08-15 18:46:19 +0200

    Add bib_item_locality + tests

--- relaton_bib/bib_item_locality.py
+++ relaton_bib/bib_item_locality.py
@@ -1,6 +1,12 @@
-from dataclasses import dataclass
-from enum import Enum
+import re
 import logging
+import xml.etree.ElementTree as ET
+
+from dataclasses import dataclass, field, fields
+from enum import Enum
+from typing import Optional
+
+from .relaton_bib import single_element_array
 
 
 class BibItemLocalityType(Enum):
@@ -23,7 +29,7 @@ class BibItemLocalityType(Enum):
 
     @classmethod
     def has_value(cls, value):
-        return value in cls._value2member_map_ 
+        return value in cls._value2member_map_
 
 
 @dataclass(frozen=True)
@@ -31,84 +37,79 @@ class BibItemLocality:
     """Bibliographic item locality."""
 
     type: str
-    reference_from: str = text()
-    reference_to: str = text(None)
+    reference_from: str
+    reference_to: Optional[str] = None
 
     def __post_init__(self):
         if not (BibItemLocalityType.has_value(self.type)
-                or re.match("locality:[a-zA-Z0-9_]+", self.type)):
-            logging.warn(f"[relaton-bib] invalid locality type: {self.type}")
+                or re.match(r"locality:[a-zA-Z0-9_]+", self.type)):
+            logging.warning(f"[relaton-bib] invalid locality type: {self.type}")
 
-    # FIXME
-    # @param builder [Nokogiri::XML::Builder]
-    # def to_xml(builder)
-    #   builder.parent[:type] = type
-    #   builder.referenceFrom reference_from # { reference_from.to_xml(builder) }
-    #   builder.referenceTo reference_to if reference_to
-    # end
+    def to_xml(self, parent):
+        parent.attrib["type"] = self.type
+        ET.SubElement(parent, "referenceFrom").text = self.reference_from
+        if self.reference_to:
+            ET.SubElement(parent, "referenceTo").text = self.reference_to
+        return parent
 
     # @param prefix [String]
     # @param count [Integeg] number of localities
     # @return [String]
-    def to_asciibib(prefix="", count=1):
+    def to_asciibib(self, prefix="", count=1):
         pref = prefix + "." if prefix else prefix
-        out = f"{prefix}::\n" if count > 1 else ""
-        out += f"{pref}type:: {self.type}\n"
-        out += f"{pref}reference_from:: {self.reference_from}\n"
-        if reference_to:
-            out += f"{pref}reference_to:: {self.reference_to}\n"
-        return out
+        out = [f"{prefix}::"] if count > 1 else []
+        out.append(f"{pref}type:: {self.type}")
+        out.append(f"{pref}reference_from:: {self.reference_from}")
+        if self.reference_to:
+            out.append(f"{pref}reference_to:: {self.reference_to}")
+        return "\n".join(out)
 
 
-class Locality(BibItemLocality)
-# @param builder [Nokogiri::XML::Builder]
-def to_xml(builder)
-  builder.locality { |b| super(b) }
-end
-end
+@dataclass(frozen=True)
+class Locality(BibItemLocality):
 
-  class LocalityStack
-    include RelatonBib
+    def to_xml(self, parent):
+        node = ET.SubElement(parent, "locality")
+        node.attrib["type"] = self.type
+        ET.SubElement(node, "referenceFrom").text = self.reference_from
+        if self.reference_to:
+            ET.SubElement(node, "referenceTo").text = self.reference_to
+        return node
 
-    # @return [Array<RelatonBib::Locality>]
-    attr_reader :locality
 
-    # @param locality [Array<RelatonBib::Locality>]
-    def initialize(locality)
-      @locality = locality
-    end
+@dataclass(frozen=True)
+class LocalityStack:
+    locality: list[Locality]
 
-    # @param builder [Nokogiri::XML::Builder]
-    def to_xml(builder)
-      builder.localityStack do |b|
-        locality.each { |l| l.to_xml(b) }
-      end
-    end
+    def to_xml(self, parent):
+        node = ET.SubElement(parent, "localityStack")
+        for loc in locality:
+            loc.to_xml(node)
+        return node
 
-    # @returnt [Hash]
-    def to_hash
-      { "locality_stack" => single_element_array(locality) }
-    end
-  end
+    # TODO REVISIT
+    # def to_hash
+    #   { "locality_stack" => single_element_array(locality) }
+    # end
 
-  class SourceLocality < BibItemLocality
-    # @param builder [Nokogiri::XML::Builder]
-    def to_xml(builder)
-      builder.sourceLocality { |b| super(b) }
-    end
-  end
 
-  class SourceLocalityStack < LocalityStack
-    # @param builder [Nokogiri::XML::Builder]
-    def to_xml(builder)
-      builder.sourceLocalityStack do |b|
-        locality.each { |l| l.to_xml(b) }
-      end
-    end
+@dataclass(frozen=True)
+class SourceLocality(BibItemLocality):
+    def to_xml(self, parent):
+        node = ET.SubElement(parent, "sourceLocality")
+        super.to_xml(node)
+        return node
 
-    # @returnt [Hash]
-    def to_hash
-      { "source_locality_stack" => single_element_array(locality) }
-    end
-  end
-end
+
+class SourceLocalityStack(LocalityStack):
+    def to_xml(self, parent):
+        node = ET.SubElement(parent, "sourceLocalityStack")
+        for loc in locality:
+            loc.to_xml(node)
+        return node
+
+#     # TODO REVISIT
+#     def to_hash
+#       { "source_locality_stack" => single_element_array(locality) }
+#     end
+
---
+++ tests/test_bib_item_locality.py
@@ -0,0 +1,93 @@
+import dataclasses
+import inspect
+import logging
+import xml.etree.ElementTree as ET
+
+from relaton_bib.bib_item_locality import BibItemLocality, BibItemLocalityType
+
+
+def test_to_xml_all_props():
+    bib_item = BibItemLocality(
+        type=BibItemLocalityType.SECTION.value,
+        reference_from="from",
+        reference_to="to")
+
+    host = ET.Element("host")
+    result = bib_item.to_xml(host)
+
+    assert host.attrib["type"] == BibItemLocalityType.SECTION.value
+    assert host.find("./referenceFrom").text == "from"
+    assert host.find("./referenceTo").text == "to"
+
+
+def test_to_xml_only_required_props():
+    bib_item = BibItemLocality(
+        type=BibItemLocalityType.CLAUSE.value,
+        reference_from="from")
+
+    host = ET.Element("host")
+    result = bib_item.to_xml(host)
+
+    assert host.attrib["type"] == BibItemLocalityType.CLAUSE.value
+    assert host.find("./referenceFrom").text == "from"
+    assert host.find("./referenceTo") is None
+
+
+def test_invalid_locality_type(caplog):
+    invalid = "invalid"
+
+    with caplog.at_level(logging.WARNING):
+        BibItemLocality(type=invalid, reference_from="from")
+
+    assert f"[relaton-bib] invalid locality type: {invalid}" in caplog.text
+
+
+def test_to_asciibib_all_props():
+    bib_item = BibItemLocality(
+        type=BibItemLocalityType.SECTION.value,
+        reference_from="from",
+        reference_to="to")
+
+    result = bib_item.to_asciibib(prefix="test")
+
+    assert result == inspect.cleandoc("""test.type:: section
+                                      test.reference_from:: from
+                                      test.reference_to:: to""")
+
+
+def test_to_asciibib_only_required_props():
+    bib_item = BibItemLocality(
+        type=BibItemLocalityType.SECTION.value,
+        reference_from="from")
+
+    result = bib_item.to_asciibib(count=2)
+
+    print(result)
+
+    assert result == inspect.cleandoc("""::
+                                      type:: section
+                                      reference_from:: from""")
+
+
+def test_hash_all_props():
+    bib_item = BibItemLocality(
+        type=BibItemLocalityType.SECTION.value,
+        reference_from="from",
+        reference_to="to")
+
+    result = dataclasses.asdict(bib_item)
+
+    assert result["type"] == BibItemLocalityType.SECTION.value
+    assert result["reference_from"] == "from"
+    assert result["reference_to"] == "to"
+
+
+def test_hash_only_required_props():
+    bib_item = BibItemLocality(
+        type=BibItemLocalityType.SECTION.value,
+        reference_from="from")
+
+    result = dataclasses.asdict(bib_item)
+
+    assert result["type"] == BibItemLocalityType.SECTION.value
+    assert result["reference_from"] == "from"
